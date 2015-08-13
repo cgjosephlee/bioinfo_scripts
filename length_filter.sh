cbac2154bfc0eac3be4daba14dc95f3e9ade4afd
Joseph Lee <cgjosephlee@gmail.com>
2015-08-13 09:13:59 +0800

    edit length_filter.sh, remove checking $1

--- length_filter.sh
+++ length_filter.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# Hsin-Han Lee
+# Hsin-Han Lee 2015/8
 
-if [ "$#" -eq 0 ]; then
+# Enable extglob for extended pattern matching
+# shopt -s extglob
+
+function usage {
     echo "Usage: $0 [fasta file] [length cutoff]"
     echo "Tips: Set length cutoff to 0 will only make multipe lines fasta to one line"
     echo "      Default output to stdout"
     exit
+}
 
-elif [[ $1 != @(*.fa|*.fasta) ]] ; then
-    echo "ERROR: invalid file"
-    exit
+if [ "$#" -eq 0 ]; then
+	usage
+
+# elif [[ $1 != @(*.fa$|*.fasta$) ]] ; then
+#     echo "ERROR: invalid file"
+#     exit
 
 elif [ -z "$2" ] || ! [[ "$2" =~ ^-?[0-9]+$ ]]; then
-    echo "ERROR: invalid length cutoff"
-    exit
+    echo -e "ERROR: invalid length cutoff\n"
+    usage
 
 else
 awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' $1 | awk -v LEN="$2" '!/^>/ { next } { getline seq } length(seq) >= LEN { print $0 "\n" seq }'
