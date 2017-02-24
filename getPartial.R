ed9f28a982295ce3f298ed2fb8c282fd4103b057
Joseph Lee <cgjosephlee@gmail.com>
2017-02-24 16:58:59 +0800

    add a script

---
+++ getPartial.R
@@ -0,0 +1,47 @@
+# extract sequence with given coordinates
+# Hsinhan Lee, 2017
+
+# Usuage
+# 0. copy this script to where the fasta file at
+# 1. open this script with RStudio (must)
+# 2. edit the following parameters
+#    in_fa: your fasta file (supposing they are in the same directory)
+#    ID   : sequence title
+#           if (ID <- ""), default is the first sequence in fasta
+#    coord: sequence coordinates c(start, end)
+#           if start > end, output will be reverse complentment (DNA only)
+in_fa <- "test.fa"
+ID <- "PNOA_0000900.1"
+coord <- c(50, 21)
+# 3. hit Ctrl+Alt+R (run all!), or Cmd+Opt+R for macOS
+# 4. find the output "sub_seq.fasta"
+
+### main ###
+if (!require("Biostrings", character.only = TRUE))
+{
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("Biostrings")
+  if(!require("Biostrings", character.only = TRUE)) stop("Package not found")
+}
+
+# edit by your favor if your are smart
+out_fa <- "sub_seq.fasta"
+path <- dirname(rstudioapi::getActiveDocumentContext()$path) # where the script at
+
+fa <- readBStringSet(filepath = file.path(path, in_fa))
+start <- coord[1]; end <- coord[2]
+
+if(ID == ""){
+  ID <- names(fa[1])
+}
+
+if(start < end){ # normal situation
+  out_seq <- subseq(fa[ID], start, end)
+  names(out_seq) <- paste0(ID, ":", start, "-", end, ";len=", width(out_seq))
+} else if(start > end){ # only support DNA strings
+  fa <- DNAStringSet(fa)
+  out_seq <- reverseComplement(subseq(fa[ID], end, start))
+  names(out_seq) <- paste0(ID, "_rev:", start, "-", end, ";len=", width(out_seq))
+}
+
+writeXStringSet(out_seq, filepath = file.path(path, out_fa))
--- getSeq.R
+++ getSeq.R
@@ -1,27 +1,28 @@
-# extract sequences from a fasta file
+# extract sequences with given IDs
 # Hsinhan Lee, 2017
 
 # Usuage
 # 0. copy this script to where the fasta file at
-# 1. open this script with RStudio (recommand)
+# 1. open this script with RStudio (must)
 # 2. edit the following two filenames (supposing they are in the same directory)
-#    in_fa: your fasta file
+#    in_fa      : your fasta file
 #    wanted_list: a text file with IDs, one ID a line
-in_fa <- "test.aa.fa"
+in_fa <- "test.fa"
 wanted_list <- "test.list.txt"
 # 3. hit Ctrl+Alt+R (run all!), or Cmd+Opt+R for macOS
 # 4. find the output "filter.fasta"
 
-# install the reguired library, only run at the fisrt time
-# source("https://bioconductor.org/biocLite.R")
-# biocLite("Biostrings")
-
 ### main ###
-library(Biostrings)
+if (!require("Biostrings", character.only = TRUE))
+{
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("Biostrings")
+  if(!require("Biostrings", character.only = TRUE)) stop("Package not found")
+}
 
 # edit by your favor if your are smart
 out_fa <- "filtered.fasta"
-path <- getwd() # where the script at
+path <- dirname(rstudioapi::getActiveDocumentContext()$path) # where the script at
 
 fa <- readBStringSet(filepath = file.path(path, in_fa))
 readlist <- file(file.path(path, wanted_list), open = "r")
