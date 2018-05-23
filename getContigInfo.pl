7fb554a75d24c4882829b7b1615c923d88fbc3f9
Joseph Lee <cgjosephlee@gmail.com>
2018-05-23 10:05:33 +0800

    update getContigInfo.pl

--- getContigInfo.pl
+++ getContigInfo.pl
@@ -6,13 +6,13 @@ use IO::File;
 use IO::Handle;
 sub usage {
 print  <<EOF;
-Usage: $0 <contigs.fa> <minimum contig length>
+Usage: $0 <contigs.fa> <minimum contig length [200]>
 EOF
 exit;
 }
 !@ARGV && &usage();
 
-my $minLength = $ARGV[1];
+my $minLength = $ARGV[1] || 200;
 
 my %contig_length = ();
 my %contig_n = ();
@@ -28,7 +28,7 @@ while (my $line = $fh->getline) {
 	} 
         else {
 		$contig_length{$contig_id} += length $line;
-		$contig_n{$contig_id} += $line =~ tr/N/N/;
+		$contig_n{$contig_id} += $line =~ tr/[Nn]/N/;
 	}
 }
 
@@ -51,26 +51,39 @@ while (my ($k,$v) = each %contig_length) {
 }
 
 #my @length_order = sort {$a<=>$b} values(%contig_length);
-my @keys = sort { $contig_length{$a} <=> $contig_length{$b} } keys(%contig_length);
+my @keys = sort { $contig_length{$b} <=> $contig_length{$a} } keys(%contig_length);
 my @length_order = @contig_length{@keys};
+my $N90 = 0;
+my $N90_ctg = 0;
 my $N50 = 0;
 my $N50_ctg = 0;
-for(my $i = 0;$i<=$#length_order;$i++) {
+for(my $i = 0; $i <= $#length_order; $i++) {
     $length_order[$i] < $minLength and next;
-    $N50 += $length_order[$i];
-    if($N50 > ($total_read_length / 2)) {
-        $N50_ctg = $i - 1;
+    $N90 += $length_order[$i];
+    if($N90 > ($total_read_length * 0.9) and $N90_ctg == 0) {
+        $N90_ctg = $i;
         last;
+    }
+    $N50 += $length_order[$i];
+    if($N50 > ($total_read_length * 0.5) and $N50_ctg == 0) {
+        $N50_ctg = $i;
     }
 }
 
-print "\nminimum length: $length_order[0] ($keys[0])\n";
-print "maximum length: $length_order[$#length_order] ($keys[$#length_order])\n";
-print "2nd long contig: $length_order[$#length_order-1] ($keys[$#length_order-1])\n";
-print "3rd long contig: $length_order[$#length_order-2] ($keys[$#length_order-2])\n";
+my $n_content=sprintf("%.3f",$n_count/$total_read_length*100);
+
+print "\ninput fasta file: $ARGV[0]\n";
+print "minimum length: $length_order[-1] ($keys[-1])\n";
+print "maximum length: $length_order[0] ($keys[0])\n";
+print "2nd long contig: $length_order[1] ($keys[1])\n" if $#length_order > 1;
+print "3rd long contig: $length_order[2] ($keys[2])\n" if $#length_order > 2;
 print "total length: $total_read_length\n";
-print "number of Ns: $n_count\n";
+print "number of Ns: $n_count ($n_content%)\n";
+#print "N content: $n_content%\n";
 print "avg. length: ".int ($total_read_length/$contig_count)."\n";
+print "L90: ",$N90_ctg+1," ($keys[$N90_ctg])\n";
+print "N90: $length_order[$N90_ctg]\n";
+print "L50: ",$N50_ctg+1," ($keys[$N50_ctg])\n";
 print "N50: $length_order[$N50_ctg]\n\n";
 
 print "contig counts:\n";
