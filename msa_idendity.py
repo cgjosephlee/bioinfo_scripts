2bb9bb7f6c26adfc05bb5cb5572a14af284e9bff
Joseph Lee <cgjosephlee@gmail.com>
2018-11-01 14:09:47 +0800

    update msa_idendity.py

--- msa_idendity.py
+++ msa_idendity.py
@@ -19,14 +19,14 @@ def usage():
     parser.add_argument('-t', type=int, default=1, help='threads for alignment (default: 1)')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-tg', action='store_true', help='output wide table for global identities (default: long table)')
-    group.add_argument('-tl', action='store_true', help='output wide table for local identities (default: long table)')
+    group.add_argument('-tl', action='store_true', help='output wide table for local (gapless) identities (default: long table)')
     return parser.parse_args()
 
 def pair_identity(s1, s2):
     '''
     convert N into gap, and ignore positions consisting only gaps in both sequences
     global: consider gap in either one of sequences as mismatch
-    local: ignore all gaps
+    local: ignore all gaps (gapless)
     '''
     aln_len = len(s1)
     s1_len = len(s1.seq.ungap('-'))
@@ -73,70 +73,74 @@ def worker():
         s1, s2, n = queue.get()
         outs[n] = pairwise_alignment(s1, s2)
 
-args = usage()
-IN = args.fasta
-NUM_THREADS = args.t
+def main():
+    args = usage()
+    IN = args.fasta
+    NUM_THREADS = args.t
 
-# main process
-if not args.aln:
-    FA = [x for x in SeqIO.parse(IN, 'fasta')]
-    seq_num = len(FA)
+    # main process
+    if not args.aln:
+        FA = [x for x in SeqIO.parse(IN, 'fasta')]
+        seq_num = len(FA)
 
-    job_n = 0
-    queue = Queue()
-    for n1 in range(seq_num):
-        for n2 in range(n1, seq_num):
-            queue.put([FA[n1], FA[n2], job_n])
-            job_n += 1
+        job_n = 0
+        queue = Queue()
+        for n1 in range(seq_num):
+            for n2 in range(n1, seq_num):
+                queue.put([FA[n1], FA[n2], job_n])
+                job_n += 1
 
-    total_runs = queue.qsize()
-    outs = [None] * queue.qsize()
-    threads = [Thread(target=worker) for x in range(NUM_THREADS)]
-    print('open {} threads, running {} jobs...'.format(NUM_THREADS, total_runs), file=sys.stderr)
-    for th in threads:
-        th.start()
-    for th in threads:
-        th.join()
-    print('finish!', file=sys.stderr)
-else:
-    FA = AlignIO.read(IN, 'fasta')
-    seq_num = len(FA)
+        total_runs = queue.qsize()
+        outs = [None] * queue.qsize()
+        threads = [Thread(target=worker) for x in range(NUM_THREADS)]
+        print('open {} threads, running {} jobs...'.format(NUM_THREADS, total_runs), file=sys.stderr)
+        for th in threads:
+            th.start()
+        for th in threads:
+            th.join()
+        print('finish!', file=sys.stderr)
+    else:
+        FA = AlignIO.read(IN, 'fasta')
+        seq_num = len(FA)
 
-    outs = []
-    for n1 in range(seq_num):
-        for n2 in range(n1, seq_num):
-            outs.append(pair_identity(FA[n1], FA[n2]))
+        outs = []
+        for n1 in range(seq_num):
+            for n2 in range(n1, seq_num):
+                outs.append(pair_identity(FA[n1], FA[n2]))
 
-glb_val = []
-loc_val = []
-for o in outs:
-    if o[0] != o[1]:
-        glb_val.append(o[5])
-        loc_val.append(o[7])
-glb_mean = sum(glb_val) / len(glb_val)
-loc_mean = sum(loc_val) / len(loc_val)
+    glb_val = []
+    loc_val = []
+    for o in outs:
+        if o[0] != o[1]:
+            glb_val.append(o[5])
+            loc_val.append(o[7])
+    glb_mean = sum(glb_val) / len(glb_val)
+    loc_mean = sum(loc_val) / len(loc_val)
 
-# output results
-if args.tg or args.tl:
-    df = pd.DataFrame()
-    if args.tg:
+    # output results
+    if args.tg or args.tl:
+        df = pd.DataFrame()
+        if args.tg:
+            for out in outs:
+                df.at[out[1], out[0]] = out[5]
+            s = 'global'
+            i = glb_mean
+        elif args.tl:
+            for out in outs:
+                df.at[out[1], out[0]] = out[7]
+            s = 'local'
+            i = loc_mean
+        print('# number of sequences: {}\n'
+              '# mean of {} identity: {:.4f}'.format(seq_num, s, i))
+        df.to_csv(sys.stdout, sep='\t')
+    else:
+        print('# number of sequences: {}\n'
+              '# mean of global identity: {:.4f}\n'
+              '# mean of local identity: {:.4f}\n'
+              '# seq1\tseq2\tlen1\tlen2\tglb_len\tglb_id\tloc_len\tloc_id'.format(seq_num, glb_mean, loc_mean))
         for out in outs:
-            df.at[out[1], out[0]] = out[5]
-        s = 'global'
-        i = glb_mean
-    elif args.tl:
-        for out in outs:
-            df.at[out[1], out[0]] = out[7]
-        s = 'local'
-        i = loc_mean
-    print('# number of sequences: {}\n'
-          '# mean of {} identity: {:.4f}'.format(seq_num, s, i))
-    df.to_csv(sys.stdout, sep='\t')
-else:
-    print('# number of sequences: {}\n'
-          '# mean of global identity: {:.4f}\n'
-          '# mean of local identity: {:.4f}\n'
-          '# seq1\tseq2\tlen1\tlen2\tglb_len\tglb_id\tloc_len\tloc_id'.format(seq_num, glb_mean, loc_mean))
-    for out in outs:
-        if out[0] != out[1]:
-            print('\t'.join(str(x) for x in out))
+            if out[0] != out[1]:
+                print('\t'.join(str(x) for x in out))
+
+if __name__ == '__main__':
+    main()
