26d2b3e291b74f892793e73bd3f912f62207db78
Joseph Lee <cgjosephlee@gmail.com>
2019-04-12 16:32:15 +0800

    update iterative_pilon.py

--- iterative_pilon.py
+++ iterative_pilon.py
@@ -6,7 +6,7 @@ Run pilon iterativily. Will detect finished runs.
 Require:
     java
     pilon
-    bwa
+    bwa/smalt
     samtools
 '''
 
@@ -22,15 +22,17 @@ parser.add_argument('fq1')
 parser.add_argument('fq2')
 parser.add_argument('-j', type=str, help='pilon jar', required=True)
 parser.add_argument('-m', type=int, help='max mem for java in Gb (64)', default=64)
-parser.add_argument('-p', type=str, help='output prefix (input basename)', default=None)
 parser.add_argument('-i', type=int, help='number of iterations (3)', default=3)
+parser.add_argument('-p', type=str, help='output prefix (input basename)', default=None)
 parser.add_argument('-t', type=int, help='threads (20)', default=20)
+parser.add_argument('--aligner', type=str, help='bwa or smalt (smalt)', default='smalt')
 args = parser.parse_args()
 
 fa_base = args.fasta
 fq1 = args.fq1
 fq2 = args.fq2
 pilon_jar = args.j
+aligner = args.aligner
 maxMem = args.m
 if not args.p:
     prefix = re.sub('.fa$|.fasta$', '', os.path.basename(fa_base)) + '_pilon'
@@ -50,12 +52,16 @@ for n in range(iter_n):
 
     print('Iteration {} start...'.format(n + 1), file=sys.stderr)
     if not os.path.isfile(fa_out + '.bam'):
-        sp.run(['bwa', 'index', fa_in], check=True)
-        sp.run('bwa mem -v 1 -t {} {} {} {} | samtools view -h -F 4 - | samtools sort -@ {} -O bam -o {}.bam -'.format(threads, fa_in, fq1, fq2, threads, fa_out), shell=True, check=True)
+        if aligner == 'bwa':
+            sp.run(['bwa', 'index', fa_in], check=True)
+            sp.run('bwa mem -v 1 -t {} {} {} {} | samtools sort -@ {} -O bam -o {}.bam -'.format(threads, fa_in, fq1, fq2, threads, fa_out), shell=True, check=True)
+            sp.run('rm {0}.amb {0}.ann {0}.bwt {0}.pac {0}.sa'.format(fa_in), shell=True, check=True)
+        elif aligner == 'smalt':
+            sp.run('smalt index {0} {0}'.format(fa_in), shell=True, check=True)
+            sp.run('smalt map -n {} {} {} | samtools sort -@ {} -O bam -o {}.bam -'.format(threads, fa_in, fq1, fq2, threads, fa_out), shell=True, check=True)
+            sp.run('rm {0}.sma {0}.smi'.format(fa_in), shell=True, check=True)
         sp.run('samtools index {}.bam'.format(fa_out), shell=True, check=True)
 
-    # sp.run(['java', '-Xmx64G', '-jar', pilon_jar, '--genome', fa_in, '--frags', fa_out + '.bam', '--output', fa_out, '--changes', '--threads', threads], check=True)
     sp.run('java -Xms512m -Xmx{}G -jar {} --genome {} --frags {}.bam --output {} --changes --threads {}'.format(maxMem, pilon_jar, fa_in, fa_out, fa_out, threads), shell=True, check=True)
-    sp.run('rm {0}.amb {0}.ann {0}.bwt {0}.pac {0}.sa'.format(fa_in), shell=True, check=True)
     print('Iteration {} finish!'.format(n + 1), file=sys.stderr)
     fa_out = fa_out + '.fasta'
