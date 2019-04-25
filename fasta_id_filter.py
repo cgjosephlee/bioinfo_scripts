f47e581bd4fb5936439e6c3aca531571e89337d7
Joseph Lee <cgjosephlee@gmail.com>
2019-04-25 17:33:02 +0800

    refactor fasta_id_filter.py

--- fasta_id_filter.py
+++ fasta_id_filter.py
@@ -1,56 +1,80 @@
 #!/usr/bin/env python
 
 """
-Header name filter for fasta files
-
-author: Hsin-han Lee
-date: Sep, 2015
+Sequence ID filter for fasta files.
 """
 
 import sys
-from Bio import SeqIO
+import re
+import argparse
 
-usage = """
-Usage: seq_name_filter.py [function] <in.fasta> <list.txt>
-[function]
-    keep (k)    : keep the sequences in the list
-    discard (d) : discard the sequences in the list
-One name per line in the list
-Output to stdout
-"""
+def filter_or_not(id, filter_list, include_mode, filter_mode):
+    if filter_mode == 'n':
+        id = id.split()[0]
+    if filter_mode in ('n', 'p') and id in filter_list:
+        found = True
+    elif filter_mode == 'r' and any(regex.search(id) for regex in filter_list):
+        found = True
+    else:
+        found = False
+    if found and include_mode:
+        return True
+    elif not found and not include_mode:
+        return True
+    else:
+        return False
 
+parser = argparse.ArgumentParser(description='Filter sequence ID from the list.')
+parser.add_argument('fasta')
+group1 = parser.add_mutually_exclusive_group()
+group1.add_argument('-i', metavar='list', help='included list')
+group1.add_argument('-e', metavar='list', help='excluded list')
+group2 = parser.add_mutually_exclusive_group()
+group2.add_argument('-n', help='default mode (match ID before first space)', action='store_true')
+group2.add_argument('-p', help='enable perfect match mode', action='store_true')
+group2.add_argument('-r', help='enable regular expression mode', action='store_true')
 
-def Usage():
-    print usage
-    sys.exit()
+args = parser.parse_args()
 
+fin = args.fasta
 
-def read_list():
-    LIST = open(sys.argv[3], "r")
-    names = [name.rstrip("\n") for name in LIST]
-    LIST.close()
-    return names
-
-if len(sys.argv) != 4:
-    Usage()
-
-elif sys.argv[1] in ("keep", "k"):
-    names = read_list()
-    FILE = open(sys.argv[2], "r")
-    for seq in SeqIO.parse(FILE, "fasta"):
-        if seq.id in names:
-            print ">" + seq.id
-            print seq.seq
-    FILE.close()
-
-elif sys.argv[1] in ("discard", "d"):
-    names = read_list()
-    FILE = open(sys.argv[2], "r")
-    for seq in SeqIO.parse(FILE, "fasta"):
-        if seq.id not in names:
-            print ">" + seq.id
-            print seq.seq
-    FILE.close()
-
+msg = 'Mode: '
+if args.i:
+    flist = args.i
+    include_mode = True
+    msg = msg + 'include, '
 else:
-    Usage()
+    flist = args.e
+    include_mode = False
+    msg = msg + 'exclude, '
+
+if args.p:
+    filter_mode = 'p'
+    msg = msg + 'perfect match.'
+elif args.r:
+    filter_mode = 'r'
+    msg = msg + 'regular expression.'
+else:
+    filter_mode = 'n'
+    msg = msg + 'default.'
+
+print(msg, file=sys.stderr)
+
+with open(flist) as f:
+    filter_list = [x for x in f.read().split() if not None]
+
+if filter_mode == 'r':
+    filter_list = [re.compile(x) for x in filter_list]
+else:
+    filter_list = set(filter_list)
+
+print('{} items in list.'.format(len(filter_list)), file=sys.stderr)
+
+with open(fin) as f:
+    for line in f:
+        if line.startswith('>'):
+            p = filter_or_not(line.strip()[1:], filter_list, include_mode, filter_mode)
+            if p:
+                print(line, end='')
+        elif p:
+            print(line, end='')
