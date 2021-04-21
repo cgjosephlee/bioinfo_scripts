aab466ef480b39e51991b3209d314112f2b2b766
Joseph Lee <cgjosephlee@gmail.com>
2021-04-21 14:35:11 +0800

    update run_purge_dups.sh

--- run_purge_dups.sh
+++ run_purge_dups.sh
@@ -7,12 +7,15 @@ reads=$2
 thread=16
 prefix=out
 
-# python pd_config.py asm.fasta ‘pwd’ <pb folder> <10x folder left blank> asm 
+# python pd_config.py asm.fasta ‘pwd’ <pb folder> <10x folder left blank> asm
 split_fa $pri_asm > $pri_asm.split
-minimap2 -t $thread -x asm5 -DP $pri_asm.split $pri_asm.split > $pri_asm.split.self.paf 
-minimap2 -t $thread -x map-ont $pri_asm $reads > read.$jobid.paf
-pbcstat read.*.paf 
-hist_plot.py PB.stat PB.stat.png
-calcuts PB.stat > cutoffs 2> calcults.log 
-purge_dups -2 -T cutoffs -c PB.base.cov $pri_asm.split.self.paf > dups.bed 2> purge_dups.log 
-get_seqs -p $prefix dups.bed $pri_asm.fasta
+minimap2 -t $thread -x asm5 -DP $pri_asm.split $pri_asm.split | gzip -1 > $pri_asm.split.self.paf.gz
+minimap2 -t $thread -x map-ont $pri_asm $reads | gzip -1 > reads.paf.gz
+pbcstat reads.paf.gz # produces PB.base.cov and PB.stat files
+
+wget https://github.com/dfguan/purge_dups/raw/master/scripts/hist_plot.py
+python hist_plot.py PB.stat PB.stat.png
+
+calcuts PB.stat > cutoffs 2> calcuts.log
+purge_dups -2 -T cutoffs -c PB.base.cov $pri_asm.split.self.paf.gz > dups.bed 2> purge_dups.log
+get_seqs -p $prefix dups.bed $pri_asm
