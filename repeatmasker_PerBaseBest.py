316f83729b531a8682e0a7f4accc98717abfb6d1
Joseph Lee <cgjosephlee@gmail.com>
2021-12-17 15:17:12 +0800

    update RM scripts

--- repeatmasker_PerBaseBest.py
+++ repeatmasker_PerBaseBest.py
@@ -1,30 +1,110 @@
 #!/usr/bin/env python3
 '''
-run this script after merge
+Solve overlaps and get best record for each base by descending score.
 
+Region      Score
+-----------------
+======          3
+  ======        1
+    ======      4
+      ======    2
+-----------------
+333344444422
+
+Usage:
 repeatmasker_to_bed.py ref.fa.out | sort -k1,1 -k2,2n > ref.fa.out.bed
-bedtools merge -d -1 -c 5,7 -o collapse -i ref.fa.out.bed > ref.fa.out.merge.bed
-repeatmasker_PerBaseBest.py ref.fa.out.merge.bed > ref.fa.out.merge.PerBaseBest.bed
+repeatmasker_PerBaseBest.py ref.fa.out.bed > ref.fa.out.PerBaseBest.bed
+
+Note:
+https://github.com/lh3/cgranges
+https://github.com/biocore-ntnu/ncls
+https://github.com/biocore-ntnu/pyranges
 '''
 
 import sys
+from intervaltree import Interval, IntervalTree
 
-with open(sys.argv[1]) as f:
-    for line in f:
-        line = line.strip()
-        cols = line.split()
-        c = cols[4].split(',')  # classes
-        if len(c) == 1:
-            print(line)
-            continue
-        s = [int(x) for x in cols[3].split(',')]  # scores
+def substract_region(Ast, Aed, Bst, Bed) -> list:
+    # return A - B
+    # A =====
+    # B   =====
+    if Ast < Bst and Aed <= Bed:
+        return [(Ast, Bst)]
+    # A   =====
+    # B =====
+    elif Ast >= Bst and Aed > Bed:
+        return [(Bed, Aed)]
+    # A =========
+    # B   =====
+    elif Ast < Bst and Aed > Bed:
+        return [(Ast, Bst), (Bed, Aed)]
+    # A   =====
+    # B =========
+    elif Ast >= Bst and Aed <= Bed:
+        return []
+    # A       =====
+    # B =====  or   =====
+    elif (Ast <= Bst and Aed <= Bst) or (Ast >= Bed and Aed >= Bed):
+        return [(Ast, Aed)]
+    else:
+        raise ValueError((Ast, Aed, Bst, Bed))
 
-        # i = s.index(max(s))  # only return first if multiple
-        i = [i for i, j in enumerate(s) if j == max(s)]  # return all maximum
-        if len(i) != 1 and len(set([c[x] for x in i])) != 1:
-            print(f'[WARNING]\t{line}', file=sys.stderr)
-        i = i[0]
+def substract_many_regions(Ast: int, Aed: int, BRegions: list) -> list:
+    ARegions = [(Ast, Aed)]
+    for Bst, Bed in BRegions:
+        tmp = []
+        for Ast, Aed in ARegions:
+            tmp += substract_region(Ast, Aed, Bst, Bed)
+        ARegions = tmp
+    return ARegions
 
-        cols[3] = str(s[i])
-        cols[4] = c[i]
-        print('\t'.join(cols))
+def overlaps_to_regions(ovl: set) -> list:
+    r = []
+    while ovl:
+        o = ovl.pop()  # o: Interval
+        r.append((o.begin, o.end))
+    return sorted(r)
+
+def solve_overlaps(lines: list) -> IntervalTree:
+    t = IntervalTree()
+    # insert to tree by descending score
+    lines.sort(key=lambda x: int(x[4]), reverse=True)
+    for line in lines:
+        st = int(line[1])
+        ed = int(line[2])
+        ovl = t.overlap(st, ed)
+        if ovl:
+            # remove overlapped regions and insert
+            BRegions = overlaps_to_regions(ovl)
+            ARegions = substract_many_regions(st, ed, BRegions)
+            for Ast, Aed in ARegions:
+                t.addi(Ast, Aed, line)
+        else:
+            t.addi(st, ed, line)
+    return t
+
+def generate_output(t: IntervalTree):
+    for st, ed, line in sorted(t):
+        info = "\t".join(line[3:])
+        print(f'{line[0]}\t{st}\t{ed}\t{info}')
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as f:
+        # initialize
+        for line in f:
+            line = line.strip().split()
+            lines = [line]
+            chrom = line[0]
+            break
+        # iterate
+        for line in f:
+            line = line.strip().split()
+            if line[0] == chrom:
+                lines.append(line)
+            else:
+                t = solve_overlaps(lines)
+                generate_output(t)
+                lines = [line]
+                chrom = line[0]
+        t = solve_overlaps(lines)
+        generate_output(t)
--- repeatmasker_to_bed.py
+++ repeatmasker_to_bed.py
@@ -1,20 +1,54 @@
 #!/usr/bin/env python3
 '''
-input: ref.fa.out
-output may need to be sorted
+Convert RepeatMasker output "ref.fa.out" to bed format.
+Read "ref.fa.align" and output Kimura divergence if exist.
+Output may need to be sorted.
 
+Usage:
 repeatmasker_to_bed.py ref.fa.out | sort -k1,1 -k2,2n > ref.fa.out.bed
 '''
 
 import sys
 
-with open(sys.argv[1]) as f:
+in_out = sys.argv[1]
+in_align = in_out[:-4] + '.align'
+
+try:
+    f = open(in_align)
+    has_align = True
+except FileNotFoundError:
+    has_align = False
+
+# find kimura divergence value by (ID, SW_score)
+kimuraDiv = {}
+if has_align:
+    for line in f:
+        line = line.strip().split()
+        if len(line) == 0:
+            continue
+        # key = (ID, score)
+        k = (line[-1], line[0])
+
+        # search Kimura
+        line = next(f)
+        while not line.startswith('Matrix'):
+            line = next(f)
+        line = next(f)
+        if line.startswith('Kimura'):
+            kimuraDiv[k] = line.strip()[26:]
+            next(f)
+        next(f)
+    f.close()
+
+with open(in_out) as f:
     # skip first 3 lines
     next(f)
     next(f)
     next(f)
     for line in f:
         line = line.strip().split()
+        if line[10] in ('Simple_repeat', 'Low_complexity', 'tRNA', 'rRNA'):
+            continue
         if line[8] == '+':
             s = '+'
         else:
@@ -23,6 +57,10 @@ with open(sys.argv[1]) as f:
             c, f = line[10].split('/')
         except:
             c, f = line[10], '.'
-        # mimic BED12 format, strand on 6th column
-        # chr start end ID score strand class family match
-        print(f'{line[4]}\t{int(line[5])-1}\t{line[6]}\t{line[14]}\t{line[0]}\t{s}\t{c}\t{f}\t{line[9]}')
+        # mimic BED12 format, strandness at 6th column
+        # chr start end ID score strand class family match %div (%Kimura)
+        if has_align:
+            k = kimuraDiv[(line[14], line[0])]
+            print(f'{line[4]}\t{int(line[5])-1}\t{line[6]}\t{line[14]}\t{line[0]}\t{s}\t{c}\t{f}\t{line[9]}\t{line[1]}\t{k}')
+        else:
+            print(f'{line[4]}\t{int(line[5])-1}\t{line[6]}\t{line[14]}\t{line[0]}\t{s}\t{c}\t{f}\t{line[9]}\t{line[1]}')
